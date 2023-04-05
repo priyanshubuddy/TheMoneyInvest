6e5a2f5f3d0eb2b7d8bced438fba79613736ca16
priyanshubuddy <priyanshudubey333@gmail.com>
2023-04-05 15:40:37 +0530

    removed some dependencies

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -143,7 +143,7 @@ function Footer() {
             <div className="row">
               <div className="col-12">
                 <p>
-                  © All Rights Reserved by{' '}
+                  U+00a9 All Rights Reserved by{' '}
                   <a href="#">
                     The Money Invest<i className="fa fa-love" />
                   </a>
--- components/PollingSection.jsx
+++ components/PollingSection.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-dupe-keys */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/button-has-type */
 
@@ -25,19 +27,27 @@ export default function PollingSection() {
 
   return (
     <div className="PollSection">
-      <h2 className="items-center text-xl font-bold border-b-2 border-b-slate-900 mx-10 mb-4">Your opinion for the Market </h2>
+      <h2 className="items-center text-xl font-bold border-b-2 border-b-slate-900 mx-10 mb-4">
+        Your opinion for the Market
+      </h2>
       <div className="VoteContainer container">
         <div className=" flex VoteDiv container">
           {/* <h6 className="buttonText">Positive</h6> */}
           <div className="flex">
             <button
-              className="w-10 h-10 rounded-full PositiveButton pollbuttons"
+              className="w-10  h-10 rounded-full PositiveButton pollbuttons"
+              style={{
+                backgroundImage: "url('./images/Positive_icon.jpg')",
+                width: '100%',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                width: '40px',
+                height: '40px',
+              }}
               onClick={() => handleVote('positive')}
-            >
-              <a>
-                Positive{' '}
-              </a>
-            </button>
+            />
+            <a className="self-center">Positive </a>
           </div>
           {/* <button
             className="w-10 h-10 rounded-full PositiveButton pollbuttons"
@@ -45,7 +55,7 @@ export default function PollingSection() {
           ></button> */}
           {/* <button class="w-44 h-12 border-4 border-green-500 rounded-full"></button> */}
           <ProgressBar
-            className="lg:w-64 border-2 border-green-800 rounded-2xl   PositiveBar"
+            className="lg:w-60 border-2 border-green-800 rounded-2xl   PositiveBar"
             now={positivePercentage}
             label={`${positivePercentage}% Positive`}
           />
@@ -53,14 +63,24 @@ export default function PollingSection() {
 
         <div className=" VoteDiv container">
           {/* <h6 className="buttonText">Negative</h6> */}
-          <button
-            className="w-10 h-10 rounded-full NegativeButton pollbuttons"
-            onClick={() => handleVote('negative')}
-          >
-            Negative
-          </button>
+          <div className="flex">
+            <button
+              className="w-10 h-10 rounded-full NegativeButton pollbuttons"
+              style={{
+                backgroundImage: "url('./images/Negative_icon.jpg')",
+                width: '100%',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                width: '40px',
+                height: '40px',
+              }}
+              onClick={() => handleVote('negative')}
+            />
+            <a className="self-center">Negative</a>
+          </div>
           <ProgressBar
-            className="lg:w-64 border-2 border-red-500 NegativeBar"
+            className="lg:w-60 border-2 border-red-500 NegativeBar"
             now={negativePercentage}
             label={`${negativePercentage}% Negative`}
           />
